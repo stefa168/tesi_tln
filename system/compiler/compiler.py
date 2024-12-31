785ac4b6f39905212e3545bb385bb3c31282b947
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-12-31 16:42:23 +0100

    chore: migrated more code to the compiler. now it can fine tune

--- system/compiler/compiler.py
+++ system/compiler/compiler.py
@@ -1,14 +1,18 @@
+import pandas as pd
 import torch
 import numpy as np
 from datasets import Dataset
+from pandas import DataFrame
+from sklearn.model_selection import StratifiedShuffleSplit
+from spacy.lang.ja.syntax_iterators import labels
 from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, AutoTokenizer
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 
-def run_training(model: AutoModelForSequenceClassification,
-                 tokenizer: AutoTokenizer,
-                 train_dataset: Dataset,
-                 eval_dataset: Dataset):
+def run_fine_tuning(model: AutoModelForSequenceClassification,
+                    tokenizer: AutoTokenizer,
+                    train_dataset: Dataset,
+                    eval_dataset: Dataset) -> bool:
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
         preds = np.argmax(predictions, axis=1)
@@ -59,9 +63,45 @@ def run_training(model: AutoModelForSequenceClassification,
     # todo save model correctly to a folder for the specific compilation configuration
     trainer.save_model()
 
+    return True
+
 
 def get_device() -> torch.device:
     return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def prepare_dataset(df: DataFrame, tok_column="Question", lab_column='Global Subject') -> tuple[Dataset, Dataset]:
+    labels = df[lab_column].unique()
+    id2label = {i: label for i, label in enumerate(labels)}
+    label2id = {label: i for i, label in enumerate(labels)}
+
+    # todo improve types
+    def tokenize_and_label(example: dict) -> dict:
+        question = example[tok_column]
+
+        # https://huggingface.co/docs/transformers/v4.46.2/en/main_classes/tokenizer#transformers.PreTrainedTokenizer.__call__
+        encodings = tokenizer(question, padding="max_length", truncation=True, max_length=128)
+        label = label2id[example[lab_column]]
+
+        encodings.update({'labels': label})
+
+        return encodings
+
+    # Create the stratified split for train and validation sets
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    train_index, val_index = next(split.split(df, df[lab_column]))
+    strat_train_set = df.loc[train_index]
+    strat_val_set = df.loc[val_index].reset_index(drop=True)
+
+    # Convert to Hugging Face Dataset
+    train_dataset = Dataset.from_pandas(strat_train_set)
+    eval_dataset = Dataset.from_pandas(strat_val_set)
+
+    # Tokenize the datasets
+    train_dataset = train_dataset.map(tokenize_and_label, remove_columns=train_dataset.column_names)
+    eval_dataset = eval_dataset.map(tokenize_and_label, remove_columns=eval_dataset.column_names)
+
+    return train_dataset, eval_dataset
 
 
 if __name__ == '__main__':
@@ -69,3 +109,15 @@ if __name__ == '__main__':
     print(f"Number of GPUs: {torch.cuda.device_count()}")
     print(f"Current CUDA device: {torch.cuda.current_device()}")
     print(f"Device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+
+    # todo extract the labels_qi to have it accessible by other parts of the compiler while training
+    model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=7)
+    model.to(get_device())
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+
+    # Load the dataset
+    df = pd.read_csv('../../multitask_training/data_cleaned_manual_combined.csv')
+
+    train_dataset, eval_dataset = prepare_dataset(df, tok_column="Question", lab_column="Global Subject")
+
+    run_fine_tuning(model, tokenizer, train_dataset, eval_dataset)
