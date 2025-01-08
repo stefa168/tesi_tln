5afcd5de2a0a877d3eefc6feff9604eafcbb30d7
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-01-08 01:25:10 +0100

    refactor: added docs and improved flow

--- ner_training/spacy/doccano_to_spacy.py
+++ ner_training/spacy/doccano_to_spacy.py
@@ -1,26 +1,43 @@
 import json
+from pathlib import Path
 
-def convert_doccano_to_spacy(input_file, output_file):
+
+def convert_doccano_to_spacy(input_file: Path, output_file: Path):
+    """
+    Convert Doccano JSONL format to spaCy training data format.
+
+    Args:
+        input_file (str): Path to the input JSONL file containing Doccano annotations.
+        output_file (str): Path to the output JSON file to save the converted spaCy training data.
+
+    Returns:
+        list: A list of tuples where each tuple contains a text and its corresponding entities.
+    """
     training_data = []
 
+    # Open the input file and read line by line
     with open(input_file, 'r', encoding='utf-8') as f:
+        # Each line in the file is a JSON object
         for line in f:
             data = json.loads(line)
             text = data['text']
             entities = []
+            # Extract entities from the Doccano annotation.
+            # Each entity is a tuple containing the start char, end char, and the label type.
             for label in data.get('label', []):
-                start_char = label[0]
-                end_char = label[1]
-                label_type = label[2]
+                start_char: int = label[0]
+                end_char: int = label[1]
+                label_type: str = label[2]
                 entities.append((start_char, end_char, label_type))
             training_data.append((text, {"entities": entities}))
 
-    # Save the training data
+    # Save the training data to the output file
     with open(output_file, 'w', encoding='utf-8') as out_f:
         json.dump(training_data, out_f)
 
     return training_data
 
-input_file = '../corpus/ner_labels.jsonl'
-output_file = '../corpus/spacy_training_data.json'
+
+input_file = Path('../corpus/ner_labels.jsonl')
+output_file = Path('../corpus/spacy_training_data.json')
 training_data = convert_doccano_to_spacy(input_file, output_file)
--- ner_training/spacy/ner_tests_spacy.py
+++ ner_training/spacy/ner_tests_spacy.py
@@ -1,22 +1,43 @@
+from typing import Literal
+
 import spacy
 from spacy.training.example import Example
 from spacy.util import minibatch, compounding
 import random
 import json
 
+
 def load_training_data(training_data_file):
+    """
+    Load training data from a JSON file.
+
+    Args:
+        training_data_file (str): Path to the JSON file containing training data.
+
+    Returns:
+        list: A list of training data.
+    """
     with open(training_data_file, 'r', encoding='utf-8') as f:
         training_data = json.load(f)
     return training_data
 
-def train_spacy(data, iterations, output_dir):
-    nlp = spacy.blank('en')  # Create a blank Language class
+
+def train_spacy(data, language, iterations, result_model_name, output_dir):
+    """
+    Train a spaCy NER model.
+
+        :param data: Training data in spaCy format.
+        :param iterations: Number of training iterations.
+        :param result_model_name: Name of the trained model.
+        :param output_dir: Directory to save the trained model to.
+    """
+    nlp = spacy.blank(language)  # Create a blank Language class
     if 'ner' not in nlp.pipe_names:
-        ner = nlp.add_pipe('ner', last=True)
+        ner = nlp.add_pipe('ner', last=True)  # Add NER pipeline if not present
     else:
         ner = nlp.get_pipe('ner')
 
-    # Add labels
+    # Find all possible labels and add them to the NER pipeline
     for _, annotations in data:
         for ent in annotations.get('entities'):
             ner.add_label(ent[2])
@@ -28,8 +49,9 @@ def train_spacy(data, iterations, output_dir):
         optimizer = nlp.begin_training()
 
         for itn in range(iterations):
-            random.shuffle(data)
+            random.shuffle(data)  # Shuffle the training data
             losses = {}
+            # Create batches of training data
             batches = minibatch(data, size=compounding(4.0, 32.0, 1.001))
             for batch in batches:
                 examples = []
@@ -37,20 +59,49 @@ def train_spacy(data, iterations, output_dir):
                     doc = nlp.make_doc(text)
                     example = Example.from_dict(doc, annotations)
                     examples.append(example)
+                # Update the model with the current batch
                 nlp.update(
                     examples,
-                    drop=0.5,
+                    drop=0.5,  # Dropout rate
                     sgd=optimizer,
                     losses=losses,
                 )
             print(f"Iteration {itn + 1}/{iterations} - Losses: {losses}")
 
-    # Save the model
-    if output_dir is not None:
-        nlp.to_disk(output_dir)
-        print(f"Saved model to {output_dir}")
+    save_location = f'../{output_dir}/{result_model_name}'
+
+    # Save the trained model to the specified directory
+    nlp.to_disk(save_location)
+    print(f"Saved model to {save_location}")
+
+
+def init_spacy_device(device: Literal['cpu', 'prefer_gpu', 'gpu'] = 'prefer_gpu') -> bool:
+    """
+    Initialize spaCy with the specified device.
+
+    Args:
+        device (str): Device to use for spaCy. Can be 'cpu', 'gpu', or 'prefer_gpu'.
+
+    Returns:
+        bool: True if the device was successfully initialized, False otherwise.
+    """
+    if device == 'cpu':
+        return spacy.require_cpu()
+    elif device == 'gpu':
+        return spacy.require_gpu()
+    else:
+        if not spacy.prefer_gpu():
+            print("WARNING: GPU not available. Using CPU for spaCy. Check if CUDA and Cupy are installed.")
+
+            return spacy.require_cpu()
+        else:
+            return True
+
 
 # Example usage:
 training_data_file = '../corpus/spacy_training_data.json'
 training_data = load_training_data(training_data_file)
-train_spacy(training_data, iterations=20, output_dir='ner_model')
+
+init_spacy_device()
+
+train_spacy(training_data, language="en", iterations=20, output_dir='ner_model2', result_model_name="ner_model2")
